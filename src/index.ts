19e662f6dd07ae585a8f6dbbe90fb4bafb776bbf
Charlie Andrews <charlie.andrews@valoraapp.com>
2022-08-27 17:56:58 -0700

    WIP implemented getFarmBotAPY (need to test!)

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,8 @@ import { BigNumber } from 'bignumber.js'
 import { FARM_DATA, mcUSD_ADDRESS_MAINNET } from './constants'
 import ethers from 'ethers'
 import { FarmBotValue } from './types'
-
-const FARM_BOT_ABI = require('./abis/FarmBot.json')
-const ERC20_ABI = require('./abis/ERC20.json')
+import FARM_BOT_ABI from './abis/FarmBot.json'
+import ERC20_ABI from './abis/ERC20.json'
 
 const RPC_URL = 'https://forno.celo.org'
 
@@ -51,6 +50,11 @@ async function getMetaLPUSDValueApprox(metaLP: string): Promise<BigNumber> {
     .dividedBy(new BigNumber(10).exponentiatedBy(18)) // convert from wei to eth (in this case USD)
 }
 
+const SECONDS_PER_BLOCK = 5
+const SECONDS_PER_DAY = 24 * 60 * 60
+const SECONDS_PER_YEAR = SECONDS_PER_DAY * 365
+const BLOCKS_PER_DAY = SECONDS_PER_DAY / SECONDS_PER_BLOCK
+
 /**
  * Get the APY of a farm bot.
  *
@@ -58,8 +62,35 @@ async function getMetaLPUSDValueApprox(metaLP: string): Promise<BigNumber> {
  *
  * @param farmBotAddress
  */
-function getFarmBotAPY(farmBotAddress: string): Promise<BigNumber> {
-  // TODO (use Compound events)
+async function getFarmBotAPY(farmBotAddress: string): Promise<BigNumber> {
+  const farmBotContract = new ethers.Contract(
+    farmBotAddress,
+    FARM_BOT_ABI,
+    rpcProvider,
+  )
+  const compoundEventsFilter = await farmBotContract.filters.Compound()
+  const compoundEventsThisWeek = await farmBotContract.queryFilter(
+    compoundEventsFilter,
+    -BLOCKS_PER_DAY * 7,
+    'latest',
+  )
+  if (compoundEventsThisWeek.length <= 2) {
+    throw new Error('Not enough compound events found')
+  }
+  const [prevCompoundEvent, curCompoundEvent] = compoundEventsThisWeek.slice(-2)
+  const compoundPeriodSeconds =
+    (curCompoundEvent.blockNumber - prevCompoundEvent.blockNumber) *
+    SECONDS_PER_BLOCK
+  const compoundTimesPerYear = Math.floor(
+    SECONDS_PER_YEAR / compoundPeriodSeconds,
+  )
+  if (!curCompoundEvent.args) {
+    throw new Error(`No args in compound event: ${curCompoundEvent}`)
+  }
+  const apr = new BigNumber(curCompoundEvent.args.lpStaked)
+    .div(curCompoundEvent.args.lpTotalBalance)
+    .multipliedBy(compoundTimesPerYear)
+  return getAPY(apr, compoundTimesPerYear)
 }
 
 export function getAPY(
