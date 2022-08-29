11c642b336a4609c89b5a86caa552ddd675f3672
Charlie Andrews <charlie.andrews@valoraapp.com>
2022-08-28 17:03:18 -0700

    configurable log level

--- src/log.ts
+++ src/log.ts
@@ -4,7 +4,7 @@ import PrettyStream from 'bunyan-prettystream'
 const consoleStream = new PrettyStream()
 consoleStream.pipe(process.stdout)
 
-const streams: Logger.Stream[] = [{ stream: consoleStream, level: 'info' }]
+const streams: Logger.Stream[] = [{ stream: consoleStream, level: process.env.LOG_LEVEL as Logger.LogLevel ?? 'info' }]
 
 export default Logger.createLogger({
   name: 'farm-bot-apy',
