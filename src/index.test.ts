8f479aa3c4da2daed151e0ba9c61ff15f1ad0b2a
Charlie Andrews-Jubelt <cajubelt@users.noreply.github.com>
2022-09-02 10:36:07 -0700

    feat(apy): apy and tvl getters (#2)
    tests done:
    - very light unit testing
    - spot checked TVL for base farms (for mcUSD-mcEUR farm)
    - spot checked APY for base farms (for mcUSD-mcEUR farm)
    
    TODO:
    - [ ] start running compounder on meta farms (required for meta farm APY calculation)
    - [ ] spot check TVL for meta farms
    - [ ] deploy! or publish to NPM and import from revo-interface

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,23 @@
-import { main } from 'src'
+import { BigNumber } from 'bignumber.js'
+import { _getAPYApprox } from './index'
 
-describe(main, () => {
-  it("should return 'Hello, world!'", () => {
-    expect(main()).toBe('Hello, world!')
+describe('getAPYApprox', () => {
+  it('works for monthly compounding', () => {
+    expect(
+      _getAPYApprox({
+        apr: new BigNumber(0.06), // 6%
+        compoundTimesPerYear: new BigNumber(12),
+      }).toFixed(5),
+    ).toEqual('0.06168') // from example at https://www.investopedia.com/terms/a/apy.asp
+  })
+  it('works for more than daily compounding', () => {
+    const apr = new BigNumber('0.50')
+    const compoundTimesPerYear = new BigNumber('75000')
+    expect(
+      _getAPYApprox({
+        apr,
+        compoundTimesPerYear,
+      }).toFixed(5),
+    ).toEqual('0.64816') // estimate should be same as daily compounding
   })
 })
