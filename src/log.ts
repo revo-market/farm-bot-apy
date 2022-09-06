54089c890dbaa041bf49830b5025573224343ef2
Charlie Andrews <charlie.andrews@valoraapp.com>
2022-09-05 21:45:40 -0700

    fix(log): remove prettystream
    says in the readme that it's not for production environments anyway

--- src/log.ts
+++ src/log.ts
@@ -1,17 +1,5 @@
 import * as Logger from 'bunyan'
-import PrettyStream from 'bunyan-prettystream'
-
-const consoleStream = new PrettyStream()
-consoleStream.pipe(process.stdout)
-
-const streams: Logger.Stream[] = [
-  {
-    stream: consoleStream,
-    level: (process.env.LOG_LEVEL as Logger.LogLevel) ?? 'info',
-  },
-]
 
 export default Logger.createLogger({
   name: 'farm-bot-apy',
-  streams,
 })
